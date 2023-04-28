29dae65fc006266e827d57a7d1688a211fb8049c
bharris6 <44404492+bharris6@users.noreply.github.com>
2023-04-28 11:45:40 -0400

    Added option to ignore known_hosts

--- src/send_to_remote.py
+++ src/send_to_remote.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python3
 import argparse
 
-from paramiko import SSHClient
+from paramiko import SSHClient, AutoAddPolicy
 from scp import SCPClient
 
 
-def send_to_remote(filepath, remote_host, username, key_file=None, remote_path="."):
- 
-     with SSHClient() as sshconn:
-         sshconn.load_system_host_keys()
-         if key_file:
-             sshconn.connect(remote_host,
-                             username=username,
-                             key_filename=key_file)
-         else:
-             sshconn.connect(remote_host,
-                             username=username)
- 
-         with SCPClient(sshconn.get_transport(), socket_timeout=90.0) as scpconn:
-             scpconn.put(filepath, remote_path.encode())
+def send_to_remote(filepath, remote_host, username, key_file=None, remote_path=".", insecure=True):
 
+    with SSHClient() as sshconn:
+        sshconn.load_system_host_keys()
+
+        if insecure:
+            sshconn.set_missing_host_key_policy(AutoAddPolicy)
+
+        if key_file:
+            sshconn.connect(remote_host,
+                            username=username,
+                            key_filename=key_file)
+        else:
+            sshconn.connect(remote_host,
+                            username=username)
+
+        with SCPClient(sshconn.get_transport(), socket_timeout=90.0) as scpconn:
+            scpconn.put(filepath, remote_path.encode())
 
 if __name__ == '__main__':
 
@@ -31,6 +34,12 @@ if __name__ == '__main__':
                         type=str,
                         required=True,
                         help="Path to file to tranfer.")
+    
+    parser.add_argument("-i",
+                        "--insecure",
+                        action="store_true",
+                        default=False,
+                        help="Whether to ignore known_hosts")
     
     parser.add_argument("-r",
                         "--remote-host",
@@ -59,4 +68,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    send_to_remote(args.file, args.remote_host, args.username, args.key_path, args.remote_path)
+    send_to_remote(args.file, args.remote_host, args.username, args.key_path, args.remote_path, args.insecure)
